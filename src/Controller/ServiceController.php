25c2d5c110452f166bdbbabdb7e322a84d774005
AmenAllahAmri <amenamri80@gmail.com>
2025-09-22 22:52:53 +0200

    Initial commit - Projet Symfony contrôleurs et routing

---
+++ src/Controller/ServiceController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ServiceController extends AbstractController
+{
+    #[Route('/service', name: 'app_service')]
+    public function index(): Response
+    {
+        return $this->render('service/index.html.twig', [
+            'controller_name' => 'ServiceController',
+        ]);
+    }
+
+    #[Route('/service/{name}', name: 'app_service_show')]
+    public function showService(string $name): Response
+    {
+        return $this->render('service/showService.html.twig', [
+            'name' => $name,
+        ]);
+    }
+
+    #[Route('/go-to-home', name: 'app_go_to_home')]
+    public function goToIndex(): Response
+    {
+        return $this->redirectToRoute('app_home');
+    }
+}
